096b8f764e12fd409e8ba30cf512199d63f5e2eb
griteach <griteach22@gmail.com>
2022-11-05 23:45:16 +0900

    update

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 const API_KEY = "8cea7307bfbffe23aa9a52daee2503c0";
 const BASE_PATH = "https://api.themoviedb.org/3";
+const IMAGE_PATH = "https://image.tmdb.org/t/p/original";
 
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
